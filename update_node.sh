f1a7b84136cac0bdb7a3c09a00b6859fa9745767
XDNA-Core <32768440+XDNA-Core@users.noreply.github.com>
2019-11-03 16:41:21 +0300

    v2.0.0.0

--- update_node.sh
+++ update_node.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 
-TARBALLURL="https://github.com/XDNA-Core/XDNA/releases/download/v1.3.0.0/ubuntu16.04-daemon.zip"
+TARBALLURL="https://github.com/XDNA-Core/XDNA/releases/download/v2.0.0.0/ubuntu16.04-daemon.zip"
 TARBALLNAME="ubuntu16.04-daemon.zip"
-XDNAVERSION="1.3.0.0"
+XDNAVERSION="2.0.0.0"
 
 clear
-echo "This script will update your masternode to version 1.3.0.0"
+echo "This script will update your masternode to version 2.0.0.0"
 read -p "Press Ctrl-C to abort or any other key to continue. " -n1 -s
 clear
 echo "Please enter your password to enter administrator mode:"
 sudo true
 echo "Shutting down masternode..."
 xdna-cli stop
-echo "Installing XDNA 1.3.0.0"
+echo "Installing XDNA 2.0.0.0"
 mkdir ./xdna-temp && cd ./xdna-temp
 wget $TARBALLURL
 unzip $TARBALLNAME
 sudo rm $TARBALLNAME
 yes | sudo cp -rf xdnad /usr/local/bin
 yes | sudo cp -rf xdna-cli /usr/local/bin
-yes | sudo cp -rf xdna-tx /usr/local/bin
 sudo chmod +x /usr/local/bin/xdnad
 sudo chmod +x /usr/local/bin/xdna-cli
-sudo chmod +x /usr/local/bin/xdna-tx
 cd ..
 sed -i '/^addnode/d' ~/.xdna/xdna.conf
 cat <<EOL >>  ~/.xdna/xdna.conf
@@ -34,4 +32,4 @@ read -p "Please wait at least 5 minutes for the wallet to load, then press any k
 clear
 echo "Starting masternode..." # TODO: Need to wait for wallet to load before starting...
 xdna-cli startmasternode local false
-xdna-cli masternode status
+xdna-cli getmasternodestatus
